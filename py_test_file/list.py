1f106651675f6249df357730def0ef054ac49ad2
ila <ila@Excluded.local>
2015-06-22 16:05:26 +0430

    this file replaced

--- py_test_file/list.py
+++ py_test_file/list.py
@@ -1,7 +1,14 @@
-the_list = ["a", 2, 3, 1, False, [1,2,3]]
-
-w = the_list.pop(3)
-print (the_list)
-
-
+def sillycap (s):
+	cap_list = []
+	lastpart_list= []
+	list_s = list (s)
+	mid = (len(list_s))/2  
+	first_part = list_s [:mid]
+	second_part = list_s[mid:]
+	for letter in second_part:
+		let1=letter.capitalize()
+		cap_list.append(let1)
+	lastpart_list= first_part + cap_list 
+	s = ''.join(lastpart_list)
+	return (s)
 
